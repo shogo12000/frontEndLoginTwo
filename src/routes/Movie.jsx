add192195dd153bb0b55c10975d3035492a319dc
sidney <shogo12000@hotmail.com>
2025-05-22 16:22:46 -0700

    fetch single movie details

--- src/routes/Movie.jsx
+++ src/routes/Movie.jsx
@@ -1,33 +1,40 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
- 
+
 export default function Movie() {
   const { id } = useParams();
-  const [movieData, setMovieData] = useState({})
+  const [movieData, setMovieData] = useState({});
   const navigate = useNavigate();
 
   const handleBack = () => {
     navigate(-1); // Volta para a página anterior
   };
 
-  useEffect(()=>{
-    async function fetchMovie(){
-        try{
-            const res = await fetch(`https://tmdbbackend.onrender.com/api/auth/movie/${id}`);
-            //const res = await fetch(`http://localhost:3000/api/auth/movie/${id}`);
-            const data = await res.json(); 
-            setMovieData(data);            
-        }catch(err){
-            console.error("Error ao carregar Filme: ", err)
-        }
+  useEffect(() => {
+    async function fetchMovie() {
+      try {
+        const res = await fetch(
+          `https://tmdbbackend.onrender.com/api/auth/movie/${id}`
+        );
+        //const res = await fetch(`http://localhost:3000/api/auth/movie/${id}`);
+        const data = await res.json();
+        setMovieData(data);
+      } catch (err) {
+        console.error("Error ao carregar Filme: ", err);
+      }
     }
 
     fetchMovie();
-  },[id])
+  }, [id]);
 
   return (
     <>
       <h1>{movieData.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
+        alt={title}
+        className="w-full h-auto mb-2 object-contain"
+      />
       <button onClick={handleBack}>Voltar</button>
     </>
   );
